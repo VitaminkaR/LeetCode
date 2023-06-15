9601774a76e2c59e79278824af37a6dfbed699ec
VladimirR <rvitaminka20@gmail.com>
2023-06-15 13:17:34 +0300

    Find Minimum in Rotated Sorted Array

---
+++ Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,18 @@
+//#include "headers.h"
+//
+//// FUNCTION
+//
+//int findMin(vector<int>& nums) {
+//	sort(nums.begin(), nums.end());
+//	return nums.at(0);
+//}
+//
+/////////////////////////////
+//
+//// TESTS
+//int main()
+//{
+//	// 1
+//	vector<int> nums({ 3,4,5,1,2 });
+//	cout << findMin(nums) << endl;
+//}
