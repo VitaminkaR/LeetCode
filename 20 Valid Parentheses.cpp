ede07999969a77fc5b13b9fea63b646421b0d560
VladimirR <rvitaminka20@gmail.com>
2024-08-07 01:51:07 +0300

    Create 20 Valid Parentheses.cpp

---
+++ 20 Valid Parentheses.cpp
@@ -0,0 +1,44 @@
+﻿#include "headers.h"
+
+bool isValid(string s) {
+	string expected = "";
+
+	if (s.size() == 1 || s.at(0) == ')' || s.at(0) == ']' || s.at(0) == '}')
+		return false;
+
+	for (size_t i = 0; i < s.size(); i++)
+	{
+		char x = s.at(i);
+		if (x == '(' || x == '[' || x == '{') {
+			expected += x;
+			continue;
+		}
+
+		if (x == ')' && expected.size() > 0 && expected.back() == '(') {
+			expected.pop_back();
+			continue;
+		}
+		else if (x == ')') return false;
+
+		if (x == ']' && expected.size() > 0 && expected.back() == '[') {
+			expected.pop_back();
+			continue;
+		}
+		else if (x == ']') return false;
+
+		if (x == '}' && expected.size() > 0 && expected.back() == '{') {
+			expected.pop_back();
+			continue;
+		}
+		else if (x == '}') return false;
+	}
+
+	if (expected.size() == 0)
+		return true;
+	
+	return false;
+}
+
+int main() {
+	cout << isValid("(){}}{");
+}
